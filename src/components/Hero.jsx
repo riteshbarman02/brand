57b5c85c0647b3030abf7ae45b33027556602497
riteshbarman02 <ritesbarman02@gmail.com>
2024-01-16 16:28:16 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+const HeroSection = () => {
+ return (
+    <main className="hero">
+        <div className="hero-content">
+          <h1> YOUR FEET DESERVE THE BEST</h1>
+          <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+          <div className="hero-button">
+            <button>Shop Now</button>
+            <button>Category</button>
+          </div>
+          <div className="shop-icons">
+            <p>Also Available On</p>
+            <div className="brand-icons">
+                <a href="https://www.flipkart.com/nike-downshifter-11-road-running-shoes-men/p/itmd3607823b0d01?pid=SHOGQZJ4RPGGVH8D&lid=LSTSHOGQZJ4RPGGVH8DEETLYM&marketplace=FLIPKART&q=nike&store=search.flipkart.com&srno=s_1_5&otracker=search&otracker1=search&fm=Search&iid=eca32445-d73f-48a5-af92-1505d2e46145.SHOGQZJ4RPGGVH8D.SEARCH&ppt=sp&ppn=sp&ssid=7k50vvutg00000001705398323936&qH=41fd220f05ed0d8c">
+                    <img src="/images/flipkart.png" alt="flipkart" />
+                </a>
+                <a href="https://www.amazon.in/Nike-Squash-Type-Mens-Shoes-Numeric_6/dp/B082R7Y37J/ref=sr_1_16?crid=1VKJ6ZSSKYJ39&dib=eyJ2IjoiMSJ9.q7yDvntH_Lyg1U8aCC1ab8sTPwruPt3XfTaMEOhuEmBuy0fYtF-dzaA7sD_IYb88gNGVBhd4ZtTT_5pBjpuwZR_ZRKP0PV53wAWrCNQsc_KttdsrySxW6HvXLyyeGZJCwwUIbhuYLT5E7_I2oGbbqZjVk-OzJhWDWTj089wjhYVYC1dil_3nekPoyaGGhd8MkJXp5_eSDifRgukzG4UEEfQJZqt_41XdwjOTngnXJHQ.fkV2xTo6R07y0XfRb-eoawyV0JsRfxQYWok0PZZR2q0&dib_tag=se&keywords=nike+sb&qid=1705398446&sprefix=nike+sh%2Caps%2C256&sr=8-16">
+                    <img src="/images/amazon.png" alt="amazon" />
+                </a>
+            </div>
+          </div>
+        </div>
+        <div className="hero-image">
+        <img src="/images/shoe_image.png" alt="shoe" />
+        </div>
+    </main>
+
+ );
+};
+
+export default HeroSection;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  return(
+    <nav>
+    <div className="logo">
+       <img src="/images/brand_logo.png" alt="logo"  />
+    </div>
+    <ul>
+       <li href="#">Menu</li>
+       <li href="#">Locations</li>
+       <li href="#">Contanct</li>
+       <li href="#">About</li>
+    </ul>
+    <button>login</button>
+  </nav>
+  );
+};
+
+export default Navigation;
